d693ac3c6bdff69c0743552db809b1325c43852f
christopherivy <cdi.747@gmail.com>
2022-12-14 23:07:00 -0800

    updated app and added api mapping

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from "react";
 import Navbar from "./components/layout/Navbar";
+import Users from "./components/users/Users";
+import axios from "axios";
+
 import "./App.css";
 
 class App extends Component {
+  componentDidMount() {
+    axios.get("https://api.github.com/users").then((res) => console.log(res.data));
+  }
+
   render() {
     return (
       <div className="App">
         <Navbar />
+        <div className="container">
+          <Users />
+        </div>
       </div>
     );
   }
